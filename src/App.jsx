a2dd2be12de9b6c628950da1e1d0eaa6e283de3d
tom@tom.cz <tom@tom.cz>
2026-08-21 11:14:48 +0200

    BaseVersion

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="app-container">
+      <h1>Hello World</h1>
+      <p>React aplikace je připravena a běží ve VS Code zde.</p>
+    </main>
+  );
+}
+
+export default App;
